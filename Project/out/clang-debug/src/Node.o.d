878b8e58179f928127238b019b60f357d06adf41
Abdo <abdulrahman.elhosseini01@eng-st.cu.edu.eg>
2022-12-09 17:30:05 +0200

    Starting point

---
+++ Project/out/clang-debug/src/Node.o.d
@@ -0,0 +1,6 @@
+../out/clang-debug/src/Node.o: Node.cc Node.h \
+  C:/OMNET/omnetpp-5.6.2/include\omnetpp.h
+
+Node.h:
+
+C:/OMNET/omnetpp-5.6.2/include\omnetpp.h:
---
+++ Project/src/MyMessage_m.cc
@@ -0,0 +1,538 @@
+//
+// Generated file, do not edit! Created by nedtool 5.6 from MyMessage.msg.
+//
+
+// Disable warnings about unused variables, empty switch stmts, etc:
+#ifdef _MSC_VER
+#  pragma warning(disable:4101)
+#  pragma warning(disable:4065)
+#endif
+
+#if defined(__clang__)
+#  pragma clang diagnostic ignored "-Wshadow"
+#  pragma clang diagnostic ignored "-Wconversion"
+#  pragma clang diagnostic ignored "-Wunused-parameter"
+#  pragma clang diagnostic ignored "-Wc++98-compat"
+#  pragma clang diagnostic ignored "-Wunreachable-code-break"
+#  pragma clang diagnostic ignored "-Wold-style-cast"
+#elif defined(__GNUC__)
+#  pragma GCC diagnostic ignored "-Wshadow"
+#  pragma GCC diagnostic ignored "-Wconversion"
+#  pragma GCC diagnostic ignored "-Wunused-parameter"
+#  pragma GCC diagnostic ignored "-Wold-style-cast"
+#  pragma GCC diagnostic ignored "-Wsuggest-attribute=noreturn"
+#  pragma GCC diagnostic ignored "-Wfloat-conversion"
+#endif
+
+#include <iostream>
+#include <sstream>
+#include "MyMessage_m.h"
+
+namespace omnetpp {
+
+// Template pack/unpack rules. They are declared *after* a1l type-specific pack functions for multiple reasons.
+// They are in the omnetpp namespace, to allow them to be found by argument-dependent lookup via the cCommBuffer argument
+
+// Packing/unpacking an std::vector
+template<typename T, typename A>
+void doParsimPacking(omnetpp::cCommBuffer *buffer, const std::vector<T,A>& v)
+{
+    int n = v.size();
+    doParsimPacking(buffer, n);
+    for (int i = 0; i < n; i++)
+        doParsimPacking(buffer, v[i]);
+}
+
+template<typename T, typename A>
+void doParsimUnpacking(omnetpp::cCommBuffer *buffer, std::vector<T,A>& v)
+{
+    int n;
+    doParsimUnpacking(buffer, n);
+    v.resize(n);
+    for (int i = 0; i < n; i++)
+        doParsimUnpacking(buffer, v[i]);
+}
+
+// Packing/unpacking an std::list
+template<typename T, typename A>
+void doParsimPacking(omnetpp::cCommBuffer *buffer, const std::list<T,A>& l)
+{
+    doParsimPacking(buffer, (int)l.size());
+    for (typename std::list<T,A>::const_iterator it = l.begin(); it != l.end(); ++it)
+        doParsimPacking(buffer, (T&)*it);
+}
+
+template<typename T, typename A>
+void doParsimUnpacking(omnetpp::cCommBuffer *buffer, std::list<T,A>& l)
+{
+    int n;
+    doParsimUnpacking(buffer, n);
+    for (int i=0; i<n; i++) {
+        l.push_back(T());
+        doParsimUnpacking(buffer, l.back());
+    }
+}
+
+// Packing/unpacking an std::set
+template<typename T, typename Tr, typename A>
+void doParsimPacking(omnetpp::cCommBuffer *buffer, const std::set<T,Tr,A>& s)
+{
+    doParsimPacking(buffer, (int)s.size());
+    for (typename std::set<T,Tr,A>::const_iterator it = s.begin(); it != s.end(); ++it)
+        doParsimPacking(buffer, *it);
+}
+
+template<typename T, typename Tr, typename A>
+void doParsimUnpacking(omnetpp::cCommBuffer *buffer, std::set<T,Tr,A>& s)
+{
+    int n;
+    doParsimUnpacking(buffer, n);
+    for (int i=0; i<n; i++) {
+        T x;
+        doParsimUnpacking(buffer, x);
+        s.insert(x);
+    }
+}
+
+// Packing/unpacking an std::map
+template<typename K, typename V, typename Tr, typename A>
+void doParsimPacking(omnetpp::cCommBuffer *buffer, const std::map<K,V,Tr,A>& m)
+{
+    doParsimPacking(buffer, (int)m.size());
+    for (typename std::map<K,V,Tr,A>::const_iterator it = m.begin(); it != m.end(); ++it) {
+        doParsimPacking(buffer, it->first);
+        doParsimPacking(buffer, it->second);
+    }
+}
+
+template<typename K, typename V, typename Tr, typename A>
+void doParsimUnpacking(omnetpp::cCommBuffer *buffer, std::map<K,V,Tr,A>& m)
+{
+    int n;
+    doParsimUnpacking(buffer, n);
+    for (int i=0; i<n; i++) {
+        K k; V v;
+        doParsimUnpacking(buffer, k);
+        doParsimUnpacking(buffer, v);
+        m[k] = v;
+    }
+}
+
+// Default pack/unpack function for arrays
+template<typename T>
+void doParsimArrayPacking(omnetpp::cCommBuffer *b, const T *t, int n)
+{
+    for (int i = 0; i < n; i++)
+        doParsimPacking(b, t[i]);
+}
+
+template<typename T>
+void doParsimArrayUnpacking(omnetpp::cCommBuffer *b, T *t, int n)
+{
+    for (int i = 0; i < n; i++)
+        doParsimUnpacking(b, t[i]);
+}
+
+// Default rule to prevent compiler from choosing base class' doParsimPacking() function
+template<typename T>
+void doParsimPacking(omnetpp::cCommBuffer *, const T& t)
+{
+    throw omnetpp::cRuntimeError("Parsim error: No doParsimPacking() function for type %s", omnetpp::opp_typename(typeid(t)));
+}
+
+template<typename T>
+void doParsimUnpacking(omnetpp::cCommBuffer *, T& t)
+{
+    throw omnetpp::cRuntimeError("Parsim error: No doParsimUnpacking() function for type %s", omnetpp::opp_typename(typeid(t)));
+}
+
+}  // namespace omnetpp
+
+
+// forward
+template<typename T, typename A>
+std::ostream& operator<<(std::ostream& out, const std::vector<T,A>& vec);
+
+// Template rule which fires if a struct or class doesn't have operator<<
+template<typename T>
+inline std::ostream& operator<<(std::ostream& out,const T&) {return out;}
+
+// operator<< for std::vector<T>
+template<typename T, typename A>
+inline std::ostream& operator<<(std::ostream& out, const std::vector<T,A>& vec)
+{
+    out.put('{');
+    for(typename std::vector<T,A>::const_iterator it = vec.begin(); it != vec.end(); ++it)
+    {
+        if (it != vec.begin()) {
+            out.put(','); out.put(' ');
+        }
+        out << *it;
+    }
+    out.put('}');
+    
+    char buf[32];
+    sprintf(buf, " (size=%u)", (unsigned int)vec.size());
+    out.write(buf, strlen(buf));
+    return out;
+}
+
+MyMessage_Base::MyMessage_Base(const char *name, short kind) : ::omnetpp::cPacket(name,kind)
+{
+    this->HeaderSeq_num = 0;
+    this->Trailer_parity = 0;
+    this->Frame_type = 0;
+    this->Ack_Nack_num = 0;
+}
+
+MyMessage_Base::MyMessage_Base(const MyMessage_Base& other) : ::omnetpp::cPacket(other)
+{
+    copy(other);
+}
+
+MyMessage_Base::~MyMessage_Base()
+{
+}
+
+MyMessage_Base& MyMessage_Base::operator=(const MyMessage_Base& other)
+{
+    if (this==&other) return *this;
+    ::omnetpp::cPacket::operator=(other);
+    copy(other);
+    return *this;
+}
+
+void MyMessage_Base::copy(const MyMessage_Base& other)
+{
+    this->HeaderSeq_num = other.HeaderSeq_num;
+    this->Payload = other.Payload;
+    this->Trailer_parity = other.Trailer_parity;
+    this->Frame_type = other.Frame_type;
+    this->Ack_Nack_num = other.Ack_Nack_num;
+}
+
+void MyMessage_Base::parsimPack(omnetpp::cCommBuffer *b) const
+{
+    ::omnetpp::cPacket::parsimPack(b);
+    doParsimPacking(b,this->HeaderSeq_num);
+    doParsimPacking(b,this->Payload);
+    doParsimPacking(b,this->Trailer_parity);
+    doParsimPacking(b,this->Frame_type);
+    doParsimPacking(b,this->Ack_Nack_num);
+}
+
+void MyMessage_Base::parsimUnpack(omnetpp::cCommBuffer *b)
+{
+    ::omnetpp::cPacket::parsimUnpack(b);
+    doParsimUnpacking(b,this->HeaderSeq_num);
+    doParsimUnpacking(b,this->Payload);
+    doParsimUnpacking(b,this->Trailer_parity);
+    doParsimUnpacking(b,this->Frame_type);
+    doParsimUnpacking(b,this->Ack_Nack_num);
+}
+
+int MyMessage_Base::getHeaderSeq_num() const
+{
+    return this->HeaderSeq_num;
+}
+
+void MyMessage_Base::setHeaderSeq_num(int HeaderSeq_num)
+{
+    this->HeaderSeq_num = HeaderSeq_num;
+}
+
+const char * MyMessage_Base::getPayload() const
+{
+    return this->Payload.c_str();
+}
+
+void MyMessage_Base::setPayload(const char * Payload)
+{
+    this->Payload = Payload;
+}
+
+char MyMessage_Base::getTrailer_parity() const
+{
+    return this->Trailer_parity;
+}
+
+void MyMessage_Base::setTrailer_parity(char Trailer_parity)
+{
+    this->Trailer_parity = Trailer_parity;
+}
+
+int MyMessage_Base::getFrame_type() const
+{
+    return this->Frame_type;
+}
+
+void MyMessage_Base::setFrame_type(int Frame_type)
+{
+    this->Frame_type = Frame_type;
+}
+
+int MyMessage_Base::getAck_Nack_num() const
+{
+    return this->Ack_Nack_num;
+}
+
+void MyMessage_Base::setAck_Nack_num(int Ack_Nack_num)
+{
+    this->Ack_Nack_num = Ack_Nack_num;
+}
+
+class MyMessageDescriptor : public omnetpp::cClassDescriptor
+{
+  private:
+    mutable const char **propertynames;
+  public:
+    MyMessageDescriptor();
+    virtual ~MyMessageDescriptor();
+
+    virtual bool doesSupport(omnetpp::cObject *obj) const override;
+    virtual const char **getPropertyNames() const override;
+    virtual const char *getProperty(const char *propertyname) const override;
+    virtual int getFieldCount() const override;
+    virtual const char *getFieldName(int field) const override;
+    virtual int findField(const char *fieldName) const override;
+    virtual unsigned int getFieldTypeFlags(int field) const override;
+    virtual const char *getFieldTypeString(int field) const override;
+    virtual const char **getFieldPropertyNames(int field) const override;
+    virtual const char *getFieldProperty(int field, const char *propertyname) const override;
+    virtual int getFieldArraySize(void *object, int field) const override;
+
+    virtual const char *getFieldDynamicTypeString(void *object, int field, int i) const override;
+    virtual std::string getFieldValueAsString(void *object, int field, int i) const override;
+    virtual bool setFieldValueAsString(void *object, int field, int i, const char *value) const override;
+
+    virtual const char *getFieldStructName(int field) const override;
+    virtual void *getFieldStructValuePointer(void *object, int field, int i) const override;
+};
+
+Register_ClassDescriptor(MyMessageDescriptor)
+
+MyMessageDescriptor::MyMessageDescriptor() : omnetpp::cClassDescriptor("MyMessage", "omnetpp::cPacket")
+{
+    propertynames = nullptr;
+}
+
+MyMessageDescriptor::~MyMessageDescriptor()
+{
+    delete[] propertynames;
+}
+
+bool MyMessageDescriptor::doesSupport(omnetpp::cObject *obj) const
+{
+    return dynamic_cast<MyMessage_Base *>(obj)!=nullptr;
+}
+
+const char **MyMessageDescriptor::getPropertyNames() const
+{
+    if (!propertynames) {
+        static const char *names[] = { "customize",  nullptr };
+        omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+        const char **basenames = basedesc ? basedesc->getPropertyNames() : nullptr;
+        propertynames = mergeLists(basenames, names);
+    }
+    return propertynames;
+}
+
+const char *MyMessageDescriptor::getProperty(const char *propertyname) const
+{
+    if (!strcmp(propertyname,"customize")) return "true";
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    return basedesc ? basedesc->getProperty(propertyname) : nullptr;
+}
+
+int MyMessageDescriptor::getFieldCount() const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    return basedesc ? 5+basedesc->getFieldCount() : 5;
+}
+
+unsigned int MyMessageDescriptor::getFieldTypeFlags(int field) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->getFieldTypeFlags(field);
+        field -= basedesc->getFieldCount();
+    }
+    static unsigned int fieldTypeFlags[] = {
+        FD_ISEDITABLE,
+        FD_ISEDITABLE,
+        FD_ISEDITABLE,
+        FD_ISEDITABLE,
+        FD_ISEDITABLE,
+    };
+    return (field>=0 && field<5) ? fieldTypeFlags[field] : 0;
+}
+
+const char *MyMessageDescriptor::getFieldName(int field) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->getFieldName(field);
+        field -= basedesc->getFieldCount();
+    }
+    static const char *fieldNames[] = {
+        "HeaderSeq_num",
+        "Payload",
+        "Trailer_parity",
+        "Frame_type",
+        "Ack_Nack_num",
+    };
+    return (field>=0 && field<5) ? fieldNames[field] : nullptr;
+}
+
+int MyMessageDescriptor::findField(const char *fieldName) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    int base = basedesc ? basedesc->getFieldCount() : 0;
+    if (fieldName[0]=='H' && strcmp(fieldName, "HeaderSeq_num")==0) return base+0;
+    if (fieldName[0]=='P' && strcmp(fieldName, "Payload")==0) return base+1;
+    if (fieldName[0]=='T' && strcmp(fieldName, "Trailer_parity")==0) return base+2;
+    if (fieldName[0]=='F' && strcmp(fieldName, "Frame_type")==0) return base+3;
+    if (fieldName[0]=='A' && strcmp(fieldName, "Ack_Nack_num")==0) return base+4;
+    return basedesc ? basedesc->findField(fieldName) : -1;
+}
+
+const char *MyMessageDescriptor::getFieldTypeString(int field) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->getFieldTypeString(field);
+        field -= basedesc->getFieldCount();
+    }
+    static const char *fieldTypeStrings[] = {
+        "int",
+        "string",
+        "char",
+        "int",
+        "int",
+    };
+    return (field>=0 && field<5) ? fieldTypeStrings[field] : nullptr;
+}
+
+const char **MyMessageDescriptor::getFieldPropertyNames(int field) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->getFieldPropertyNames(field);
+        field -= basedesc->getFieldCount();
+    }
+    switch (field) {
+        default: return nullptr;
+    }
+}
+
+const char *MyMessageDescriptor::getFieldProperty(int field, const char *propertyname) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->getFieldProperty(field, propertyname);
+        field -= basedesc->getFieldCount();
+    }
+    switch (field) {
+        default: return nullptr;
+    }
+}
+
+int MyMessageDescriptor::getFieldArraySize(void *object, int field) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->getFieldArraySize(object, field);
+        field -= basedesc->getFieldCount();
+    }
+    MyMessage_Base *pp = (MyMessage_Base *)object; (void)pp;
+    switch (field) {
+        default: return 0;
+    }
+}
+
+const char *MyMessageDescriptor::getFieldDynamicTypeString(void *object, int field, int i) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->getFieldDynamicTypeString(object,field,i);
+        field -= basedesc->getFieldCount();
+    }
+    MyMessage_Base *pp = (MyMessage_Base *)object; (void)pp;
+    switch (field) {
+        default: return nullptr;
+    }
+}
+
+std::string MyMessageDescriptor::getFieldValueAsString(void *object, int field, int i) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->getFieldValueAsString(object,field,i);
+        field -= basedesc->getFieldCount();
+    }
+    MyMessage_Base *pp = (MyMessage_Base *)object; (void)pp;
+    switch (field) {
+        case 0: return long2string(pp->getHeaderSeq_num());
+        case 1: return oppstring2string(pp->getPayload());
+        case 2: return long2string(pp->getTrailer_parity());
+        case 3: return long2string(pp->getFrame_type());
+        case 4: return long2string(pp->getAck_Nack_num());
+        default: return "";
+    }
+}
+
+bool MyMessageDescriptor::setFieldValueAsString(void *object, int field, int i, const char *value) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->setFieldValueAsString(object,field,i,value);
+        field -= basedesc->getFieldCount();
+    }
+    MyMessage_Base *pp = (MyMessage_Base *)object; (void)pp;
+    switch (field) {
+        case 0: pp->setHeaderSeq_num(string2long(value)); return true;
+        case 1: pp->setPayload((value)); return true;
+        case 2: pp->setTrailer_parity(string2long(value)); return true;
+        case 3: pp->setFrame_type(string2long(value)); return true;
+        case 4: pp->setAck_Nack_num(string2long(value)); return true;
+        default: return false;
+    }
+}
+
+const char *MyMessageDescriptor::getFieldStructName(int field) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->getFieldStructName(field);
+        field -= basedesc->getFieldCount();
+    }
+    switch (field) {
+        default: return nullptr;
+    };
+}
+
+void *MyMessageDescriptor::getFieldStructValuePointer(void *object, int field, int i) const
+{
+    omnetpp::cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount())
+            return basedesc->getFieldStructValuePointer(object, field, i);
+        field -= basedesc->getFieldCount();
+    }
+    MyMessage_Base *pp = (MyMessage_Base *)object; (void)pp;
+    switch (field) {
+        default: return nullptr;
+    }
+}
+
+
